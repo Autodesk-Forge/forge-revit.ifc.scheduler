4ee166d3f0eda7aa58ed76604c4fae467d6f93e5
Eason Kang <eason.kang@autodesk.com>
2022-03-08 15:32:24 +0800

    Fix log not found issue.

--- Controllers/LogsController.cs
+++ Controllers/LogsController.cs
@@ -12,15 +12,31 @@ using RevitToIfcScheduler.Utilities;
 
 namespace RevitToIfcScheduler.Controllers
 {
-    public class LogsController: ControllerBase
+    public class LogsController : ControllerBase
     {
         public LogsController(Context.RevitIfcContext revitIfcContext)
         {
             RevitIfcContext = revitIfcContext;
         }
-         
+
         private Context.RevitIfcContext RevitIfcContext { get; }
-        
+
+        private string LogFolderFullPath
+        {
+            get
+            {
+                var logPath = AppConfig.LogPath;
+                // get the file attributes for file or directory
+                FileAttributes attr = System.IO.File.GetAttributes(logPath);
+                if (!attr.HasFlag(FileAttributes.Directory))
+                {
+                    logPath = new FileInfo(logPath).Directory.FullName;
+                }
+
+                return logPath;
+            }
+        }
+
         [HttpGet]
         [Route("api/logs")]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -30,10 +46,10 @@ namespace RevitToIfcScheduler.Controllers
         {
             try
             {
-                if (!Authentication.IsAuthorized(HttpContext, RevitIfcContext, new List<AccountRole>(){AccountRole.AccountAdmin, AccountRole.ApplicationAdmin})) return Unauthorized();
-                
+                if (!Authentication.IsAuthorized(HttpContext, RevitIfcContext, new List<AccountRole>() { AccountRole.AccountAdmin, AccountRole.ApplicationAdmin })) return Unauthorized();
+
                 //Get list of file names from log location
-                DirectoryInfo logFolder = new DirectoryInfo(new FileInfo(AppConfig.LogPath).Directory.FullName);
+                DirectoryInfo logFolder = new DirectoryInfo(this.LogFolderFullPath);
                 FileInfo[] Files = logFolder.GetFiles("*.txt");
 
                 List<string> fileNames = new List<string>();
@@ -42,7 +58,7 @@ namespace RevitToIfcScheduler.Controllers
                 {
                     fileNames.Add(file.Name);
                 }
-                
+
                 return Ok(fileNames);
             }
             catch (Exception ex)
@@ -51,7 +67,7 @@ namespace RevitToIfcScheduler.Controllers
                 return BadRequest(ex);
             }
         }
-        
+
         [HttpGet]
         [Route("api/logs/{fileName}")]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -61,17 +77,18 @@ namespace RevitToIfcScheduler.Controllers
         {
             try
             {
-                if (!Authentication.IsAuthorized(HttpContext, RevitIfcContext, new List<AccountRole>(){AccountRole.AccountAdmin, AccountRole.ApplicationAdmin})) return Unauthorized();
+                if (!Authentication.IsAuthorized(HttpContext, RevitIfcContext, new List<AccountRole>() { AccountRole.AccountAdmin, AccountRole.ApplicationAdmin })) return Unauthorized();
 
-                DirectoryInfo logFolder = new DirectoryInfo(new FileInfo(AppConfig.LogPath).Directory.FullName);
-                var path = $"{logFolder}\\{fileName}";
+                var path = Path.Combine(this.LogFolderFullPath, fileName);
 
                 //FileStream necessary as the latest file is currently being used by the logger
                 using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-                    
-                using (var sr = new StreamReader(fs, Encoding.Default)) {
-                    var file2 = sr.ReadToEnd();
-                    return Ok(file2);
+                {
+                    using (var sr = new StreamReader(fs, Encoding.Default))
+                    {
+                        var file2 = await sr.ReadToEndAsync();
+                        return Ok(file2);
+                    }
                 }
             }
             catch (Exception ex)
